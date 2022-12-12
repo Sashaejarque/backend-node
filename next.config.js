cb6bcb4b1d3498ae95a446c557262f5b37a2f97f
Sasha Ejarque <sashax191@gmail.com>
2022-12-12 12:57:49 -0300

    Added configuration for deploy on vercel

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+module.exports ={
+    env: {
+        MONGO_DB_CNN: process.env.MONGO_DB_CNN,
+        CLOUDINARY_URL: process.env.CLOUDINARY_URL,
+        SECRETORPRIVATEKEY: process.env.SECRETORPRIVATEKEY,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+
+    }
+}
